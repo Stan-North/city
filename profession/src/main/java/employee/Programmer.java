085f8b83fe0c592ec15baa8d7d62ceaeb875e244
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2024-10-22 22:25:56 +0300

    добавил описание методов, добавил константы

--- profession/src/main/java/employee/Programmer.java
+++ profession/src/main/java/employee/Programmer.java
@@ -7,6 +7,7 @@ import task.ProgrammerTask;
 import java.math.BigDecimal;
 
 public class Programmer extends Employee implements ProgrammerTask {
+    private static final int COMPARE_SEPARATOR = 0;
     private static final BigDecimal MIN_RATE = new BigDecimal(1500);
     private static final BigDecimal MAX_RATE = new BigDecimal(2000);
     private static final String MIN_RATE_EXCEPTION = "Часовая ставка ниже минимальной < ";
@@ -17,12 +18,15 @@ public class Programmer extends Employee implements ProgrammerTask {
         super(firstName, lastName, middleName, gender);
     }
 
+    /**
+     * Устанавливает ставку в заданных границах минимальной и максимальнйо ставки
+     */
     @Override
     public void setRate(BigDecimal rate) throws EmployeeInvalidRateException {
-        if (rate.compareTo(MIN_RATE) < 0) {
+        if (rate.compareTo(MIN_RATE) < COMPARE_SEPARATOR) {
             throw new EmployeeInvalidRateException(MIN_RATE_EXCEPTION + MIN_RATE);
         }
-        if (rate.compareTo(MAX_RATE) > 0) {
+        if (rate.compareTo(MAX_RATE) > COMPARE_SEPARATOR) {
             throw new EmployeeInvalidRateException(MAX_RATE_EXCEPTION + MAX_RATE);
         }
         super.setRate(rate);
--- profession/src/main/java/task/ProgrammerTask.java
+++ profession/src/main/java/task/ProgrammerTask.java
@@ -3,13 +3,20 @@ package task;
 import lombok.NonNull;
 
 public interface ProgrammerTask {
+    Integer INITIAL_PROGRAMMER_TASK_LABOR_HOURS = 0;
     String REDUCED_LABOR_HOURS_EXCEPTION = "Часы работы нельзя уменьшить";
 
+    /**
+     * Задача взята в работу, переводится в статус "в работе", устанавливаются часы в ноль.
+     */
     default void takeTask(@NonNull Task task) {
         task.setStatus(TaskStatusType.IN_PROGRESS);
-        task.setLaborHours(0);
+        task.setLaborHours(INITIAL_PROGRAMMER_TASK_LABOR_HOURS);
     }
 
+    /**
+     * Задача сделана, переводится в статус "завершена", устанавливаются часы, но не меньше имеющихся по задаче.
+     */
     default void doneTask(@NonNull Task task, @NonNull Integer laborHours) throws LaborHoursException {
         if (laborHours < task.getLaborHours()) {
             throw new LaborHoursException(REDUCED_LABOR_HOURS_EXCEPTION);
