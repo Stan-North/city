141dd2b24f9a2a815b9667c8635baab4685fec00
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2024-10-23 20:37:20 +0300

    отрефакторил код

--- profession/src/main/java/employee/Programmer.java
+++ profession/src/main/java/employee/Programmer.java
@@ -7,11 +7,12 @@ import lombok.experimental.FieldDefaults;
 
 import java.math.BigDecimal;
 
+import static employee.ProgrammerRateRange.MAX_RATE;
+import static employee.ProgrammerRateRange.MIN_RATE;
+
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class Programmer extends Employee {
     static int COMPARE_SEPARATOR = 0;
-    static BigDecimal MIN_RATE = new BigDecimal(1500);
-    static BigDecimal MAX_RATE = new BigDecimal(2000);
     static String MIN_RATE_EXCEPTION = "Часовая ставка ниже минимальной < ";
     static String MAX_RATE_EXCEPTION = "Часовая ставка выше максимальной > ";
     static Double INITIAL_PROGRAMMER_TASK_LABOR_HOURS = 0.0;
@@ -26,14 +27,22 @@ public class Programmer extends Employee {
      * Устанавливает ставку в заданных границах минимальной и максимальнйо ставки
      */
     @Override
-    public void setRate(BigDecimal rate) throws EmployeeInvalidRateException {
-        if (rate.compareTo(MIN_RATE) < COMPARE_SEPARATOR) {
-            throw new EmployeeInvalidRateException(MIN_RATE_EXCEPTION + MIN_RATE);
-        }
-        if (rate.compareTo(MAX_RATE) > COMPARE_SEPARATOR) {
-            throw new EmployeeInvalidRateException(MAX_RATE_EXCEPTION + MAX_RATE);
-        }
+    public void setRate(BigDecimal rate) throws EmployeeInvalidRateException  {
+        checkRateInRange(rate);
         super.setRate(rate);
+    }
+
+    /**
+     * Проверяет границы ставки в заданных границах минимальной и максимальнйо ставки
+     */
+
+    private void checkRateInRange(BigDecimal rate) throws EmployeeInvalidRateException {
+        if (rate.compareTo(MIN_RATE.getRate()) < COMPARE_SEPARATOR) {
+            throw new EmployeeInvalidRateException(MIN_RATE_EXCEPTION + MIN_RATE.getRate());
+        }
+        if (rate.compareTo(MAX_RATE.getRate()) > COMPARE_SEPARATOR) {
+            throw new EmployeeInvalidRateException(MAX_RATE_EXCEPTION + MAX_RATE.getRate());
+        }
     }
 
     /**
--- profession/src/main/java/employee/Task.java
+++ profession/src/main/java/employee/Task.java
@@ -5,13 +5,15 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
+import static employee.TaskStatusType.IN_PROGRESS;
+
 
 @Setter(value = AccessLevel.PROTECTED)
 @Getter()
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Task {
     String description;
-    TaskStatusType status;
+    TaskStatusType status = IN_PROGRESS;
     Double laborHours;
 
     public Task(String description) {
