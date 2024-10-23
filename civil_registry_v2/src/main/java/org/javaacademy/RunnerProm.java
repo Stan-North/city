26ae6df2157f988cc5adcbd12b87a2c248c7d1ec
jabes <jabesgames@gmail.com>
2024-10-23 20:31:31 +0300

    Создан класс RunnerProm

---
+++ civil_registry_v2/src/main/java/org/javaacademy/RunnerProm.java
@@ -0,0 +1,44 @@
+package org.javaacademy;
+
+import human.Gender;
+import human.Human;
+import org.javaacademy.citizen.Citizen;
+import org.javaacademy.citizen.MaritalStatus;
+import org.javaacademy.civil_registry.CivilRegistry;
+
+import java.time.LocalDate;
+
+public class RunnerProm {
+    public static void main(String[] args) {
+        Citizen man = new Citizen("Иван", "Иванов1", "Иванович",
+                Gender.MALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen woman = new Citizen("Екатерина", "Петрова", "Ивановна",
+                Gender.FEMALE, MaritalStatus.NOT_MARRIED, null);
+        Human child = man.makeChild("Петр", "Иванов",
+                "Иванович", Gender.MALE, woman);
+        Citizen childCitizen = new Citizen(child);
+
+        Citizen man2 = new Citizen("Иван", "Иванов2", "Иванович",
+                Gender.MALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen man3 = new Citizen("Иван", "Иванов3", "Иванович",
+                Gender.MALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen woman2 = new Citizen("Екатерина", "Петрова", "Ивановна",
+                Gender.FEMALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen woman3 = new Citizen("Екатерина", "Петрова", "Ивановна",
+                Gender.FEMALE, MaritalStatus.NOT_MARRIED, null);
+
+        CivilRegistry civilRegistry = new CivilRegistry("args[0]");
+
+        civilRegistry.marriageRegistration(man, woman, LocalDate.now());
+        civilRegistry.marriageRegistration(man2, woman2, LocalDate.now());
+
+
+        civilRegistry.divorceRegistration(man2, woman2, LocalDate.now());
+
+        civilRegistry.childRegistration(childCitizen, man, woman, LocalDate.now());
+        civilRegistry.childRegistration(childCitizen, man, woman, LocalDate.now());
+        civilRegistry.childRegistration(childCitizen, man, woman, LocalDate.now());
+
+        civilRegistry.statisticsOfDate(LocalDate.now());
+    }
+}
