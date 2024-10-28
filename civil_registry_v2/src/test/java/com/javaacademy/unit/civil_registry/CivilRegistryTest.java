c7e5bc42c9432cdb18ea5559a8916be84ba04d9d
Aleksandr Akimov <gtwood@yandex.ru>
2024-10-28 21:23:29 +0300

    коммит перед мержем

--- civil_registry_v2/src/test/java/com/javaacademy/unit/civil_registry/CivilRegistryTest.java
+++ civil_registry_v2/src/test/java/com/javaacademy/unit/civil_registry/CivilRegistryTest.java
@@ -1,22 +1,69 @@
 package com.javaacademy.unit.civil_registry;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.javaacademy.citizen.Citizen;
 import org.javaacademy.citizen.MaritalStatus;
-import org.javaacademy.civil_registry.CitizenIsMarriedException;
 import org.javaacademy.civil_registry.CivilActionRecord;
-import org.javaacademy.civil_registry.CivilActionType;
 import org.javaacademy.civil_registry.CivilRegistry;
 import org.javaacademy.human.Gender;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.mockito.ArgumentCaptor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 @DisplayName("Тестирование класса ЗАГС(CivilRegistry)")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CivilRegistryTest {
+    String civilRegistryName;
+    CivilRegistry civilRegistry;
+    LocalDate date;
+    Citizen man;
+    Citizen woman;
+    Citizen child;
 
+    @BeforeEach
+    void setUp() {
+        civilRegistryName = "ЗАГС № 15";
+        civilRegistry = new CivilRegistry(civilRegistryName);
+        LocalDate date = LocalDate.now();
+        Citizen man = new Citizen("Иван", "Иванов1", "Иванович",
+                Gender.MALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen woman = new Citizen("Екатерина", "Петрова", "Ивановна",
+                Gender.FEMALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen child = man.makeChild("Петр", "Иванов",
+                "Иванович", Gender.MALE, woman);
+    }
+
+    @Test
+    @DisplayName("Проверка конструктора ЗАГСА")
+    void checkCivilRegistry() {
+        Assertions.assertEquals(civilRegistryName, civilRegistry.getCivilRegistryName(),
+                "Имема загса не соответствует");
+    }
+
+//    childRegistration
+
+    @Test
+    @DisplayName("Успешная проверка полей ребенка-гражданина")
+    public void childRegistrationSuccess() {
+        LocalDate date = LocalDate.now();
+        Citizen man = new Citizen("Иван", "Иванов1", "Иванович",
+                Gender.MALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen woman = new Citizen("Екатерина", "Петрова", "Ивановна",
+                Gender.FEMALE, MaritalStatus.NOT_MARRIED, null);
+        Citizen child = man.makeChild("Петр", "Иванов",
+                "Иванович", Gender.MALE, woman);
+
+
+    }
+
+//    marriageRegistration
+
+//    divorceRegistration
 
 }
