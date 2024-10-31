ec5eda377d6ab2bdc5be66cf065cb83ebcf96869
Stan-North <spine201150@gmail.com>
2024-10-31 19:44:08 +0300

    удалил старый неиспользуемый класс c эксепшеном, который остался после конфликтов

--- civil_registry/src/main/java/org/javaacademy/citizen/SponceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.javaacademy.citizen;
-
-public class SponceNotFoundException extends RuntimeException {
-    public SponceNotFoundException(String message) {
-        super(message);
-    }
-}
